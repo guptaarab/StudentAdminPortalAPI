4e08dcc4535f06f9c011d7476bdc2af8921e3839
guptaarab <guptaarab10022001@gmail.com>
2023-06-01 11:12:46 +0530

    First Api

--- StudentAdminPortalAPI/Program.cs
+++ StudentAdminPortalAPI/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using StudentAdminPortalAPI.DataModels;
+using StudentAdminPortalAPI.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<StudentAdminContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("StudentAdminPortalDb")));
+
+builder.Services.AddScoped<IStudentRepository, SqlStudentRepository>();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
---
+++ StudentAdminPortalAPI/Repositories/SqlStudentRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentAdminPortalAPI.DataModels;
+
+namespace StudentAdminPortalAPI.Repositories
+{
+    public class SqlStudentRepository : IStudentRepository
+    {
+        private readonly StudentAdminContext context;
+        public SqlStudentRepository(StudentAdminContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Student>> GetStudentsAsync()
+        {
+            return await context.Student.Include(s => s.Gender).Include(s => s.Address).ToListAsync();
+        }
+
+        
+    }
+}
